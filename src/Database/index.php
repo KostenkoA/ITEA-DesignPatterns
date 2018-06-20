d7d17d1d5e8e149f98d9aa1127c12d3133bdfae2
KostenkoA <kostenkoam1212@gmail.com>
2018-06-20 23:06:31 +0300

    Added Fluent Interface

--- src/Database/index.php
+++ src/Database/index.php
@@ -5,11 +5,16 @@ use App\DesignPatterns\Database\SqlQueryBuilder;
 require __DIR__ . '/../../vendor/autoload.php';
 
 $queryBuilder = new SqlQueryBuilder();
-
+/*
 $queryBuilder->select('id, first_name');
 $queryBuilder->from('user');
-$queryBuilder->where('yers>30');
+$queryBuilder->where('years>30');
 
 $query = $queryBuilder->getQuery();
+*/
 
+$query = $queryBuilder->select('id')
+    ->from('user')
+    ->where('years>30')
+    ->getQuery();
 var_dump($query);
